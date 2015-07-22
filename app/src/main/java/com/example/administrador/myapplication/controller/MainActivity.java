a903d496d64089fab6b159fe09e6eefb1fcc6b20
200 <g_ta_07@hotmail.com>
2015-07-21 21:10:46 -0300

    Commit 21/07/2015

--- app/src/main/java/com/example/administrador/myapplication/controller/MainActivity.java
+++ app/src/main/java/com/example/administrador/myapplication/controller/MainActivity.java
@@ -17,30 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Client> clientNames = getClients();
+        List<Client> clientNames = Client.getAll();
 
 
         ListView listViewClients = (ListView) findViewById(R.id.listViewCliente);
 
         ClientListAdapter clientAdapter = new ClientListAdapter(MainActivity.this, clientNames);
 
-
         listViewClients.setAdapter(clientAdapter);
-    }
-
-    private List<Client> getClients() {
-        List<Client> clients = new ArrayList<>();
-
-        Client duzentos = new Client();
-        duzentos.setName("Duzentos");
-        duzentos.setAge(21);
-        clients.add(duzentos);
-
-        Client valdeco = new Client();
-        valdeco.setName("Valdeco");
-        valdeco.setAge(26);
-        clients.add(valdeco);
-
-        return clients;
     }
 }
